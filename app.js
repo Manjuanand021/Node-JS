5df136f754b383d922f011254bf5c8bb92df8d5f
Manjuanand <mtoladinni@tradeking.com>
2016-12-24 23:34:11 -0500

    other examples

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 'use strict';
+class Person{
+    constructor(firstName, lastName){
+        this.fName = firstName || 'Manjuanand';
+        this.lName = lastName || 'Toladinni';
+    }
+
+    greet(){
+        console.log(`Hello Mr. ${this.fName} ${this.lName}`);
+    }
+}
+
+class Employee extends Person{
+    constructor(num){
+        super();
+        this.empNum = num;
+    }
+}
+
+let e1 = new Employee('130787');
+e1.greet();
